b771ba1550328aa15f20696e414a11f4b7971ebd
Leif-W <Leif-W@users.noreply.github.com>
2020-10-11 08:38:54 -0400

    urlencoded set express=true to silence deprecation

--- biz_dashboard/index.js
+++ biz_dashboard/index.js
@@ -18,25 +18,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ express: true }));
 app.use(cookieParser());
 app.use(express.static('assets'));
 //app.use(expresLayouts);
-app.use(session({
-    name : 'bizdash',
-    secret : 'blahsomething',
-    saveUninitialized : false,
-    resave : false,
-    cookie : {
-        maxAge : (1000*60*100)
+app.use(
+  session({
+    name: 'bizdash',
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
     },
-    store : new mongoStore({
-        mongooseConnection : db,
-        autoRemove : 'disabled'
-    }, function(err) {
+    store: new mongoStore(
+      {
+        mongooseConnection: db,
+        autoRemove: 'disabled',
+      },
+      function (err) {
         console.log(err || 'connect-mongo-setup is ok');
-    })
-}));
+      }
+    ),
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(passport.setAuthenticatedUser);
@@ -44,10 +49,10 @@ app.use(flash());
 app.use(customMWare.setFlash);
 app.use('/', require('./routes'));
 
-app.listen(port, function(err) {
-    if (err) {
-        console.log('Error in running the server onport: '+err);
-        return;
-    }
-    console.log('Server is running perfectly on port: '+port);
-})
+app.listen(port, function (err) {
+  if (err) {
+    console.log('Error in running the server onport: ' + err);
+    return;
+  }
+  console.log('Server is running perfectly on port: ' + port);
+});
